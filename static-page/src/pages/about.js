0552a1640b80975e20dd7976e22807d1dce90247
nivyapeter <nivyakpeter@gmail.com>
2021-12-24 20:11:06 +0530

    gatsby initial set up and routes

---
+++ static-page/src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function about() {
+    return (
+        <div>
+            <h1>About page</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, tenetur!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, aliquam!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, error!</p>
+            
+        </div>
+    )
+}
