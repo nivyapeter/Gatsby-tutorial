6ed47e4f7c2c9b30c65a40755242064a0da22bbf
nivyapeter <nivyakpeter@gmail.com>
2021-12-24 23:14:46 +0530

    layout section

--- static-page/src/pages/about.js
+++ static-page/src/pages/about.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function about() {
     return (
+        <Layout> 
         <div>
             <h1>About page</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, tenetur!</p>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, aliquam!</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, error!</p>
-            
         </div>
+        </Layout>
     )
 }
--- static-page/src/pages/index.js
+++ static-page/src/pages/index.js
@@ -1,15 +1,17 @@
 import React from "react"
-import Navbar from "../components/Navbar"
+import Layout from "../components/Layout"
+
 
 export default function Home() {
   return (
     <section>
-    <Navbar />
+    <Layout>
    <div>
     <h2>Design</h2>
     <h3>Develop & Deploy</h3>
     <p>Lorem ipsum dolor sit amet.</p>
   </div>
+  </Layout>
   </section>
   )
 }
