ce6738c15eaa811941961794792c234c70021a1e
nivyapeter <nivyakpeter@gmail.com>
2022-01-04 14:39:35 +0530

    global page

--- my-project/src/components/HomeButton.js
+++ my-project/src/components/HomeButton.js
@@ -4,7 +4,7 @@ export default function HomeButton() {
     return (
         <div>
             <a href="">
-            <button className=""></button>
+            <button className="text-white bg-blue p-10"></button>
             </a>
        </div>
     )
